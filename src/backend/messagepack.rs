ceb402da6e43935c6144868a7543cdf2f1135905
Du Shiqiao <lucidfrontier.45@gmail.com>
2026-01-13 12:14:23 +0900

    refactor: simplify error handling in read and write functions

--- src/backend/messagepack.rs
+++ src/backend/messagepack.rs
@@ -5,11 +5,11 @@ use serde::{Serialize, de::DeserializeOwned};
 use crate::Error;
 
 pub fn read<T: DeserializeOwned>(reader: impl Read) -> Result<T, Error> {
-    rmp_serde::decode::from_read(reader).map_err(Error::from)
+    Ok(rmp_serde::decode::from_read(reader)?)
 }
 
 pub fn write<T: Serialize>(mut writer: impl Write, record: &T) -> Result<(), Error> {
-    rmp_serde::encode::write(&mut writer, record).map_err(Error::from)
+    Ok(rmp_serde::encode::write(&mut writer, record)?)
 }
 
 #[cfg(test)]
