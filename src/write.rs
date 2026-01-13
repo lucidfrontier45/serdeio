5ef8622f814467b34a9868480388d4ca94764844
Du Shiqiao <lucidfrontier.45@gmail.com>
2026-01-13 13:30:12 +0900

    fix: ensure MessagePack format is correctly handled in write functions

--- src/write.rs
+++ src/write.rs
@@ -13,6 +13,7 @@ pub fn write_record_to_writer<T: Serialize>(
         DataFormat::Json => backend::json::write(writer, record),
         #[cfg(feature = "yaml")]
         DataFormat::Yaml => backend::yaml::write(writer, record),
+        #[cfg(feature = "messagepack")]
         DataFormat::MessagePack => backend::messagepack::write(writer, record),
         _ => Err(Error::UnsupportedFormat(data_format)),
     }
@@ -30,6 +31,7 @@ pub fn write_records_to_writer<'a, T: Serialize + 'a>(
         DataFormat::Csv => backend::csv::write(writer, records),
         #[cfg(feature = "yaml")]
         DataFormat::Yaml => backend::yaml::write(writer, &records.into_iter().collect::<Vec<_>>()),
+        #[cfg(feature = "messagepack")]
         DataFormat::MessagePack => {
             backend::messagepack::write(writer, &records.into_iter().collect::<Vec<_>>())
         }
